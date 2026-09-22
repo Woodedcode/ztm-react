d56f6a8d40215f66eb2695a865991f3051c5eea9
Ryan Woods <Woodedcode@gmail.com>
2026-09-21 22:09:24 -0500

    learning useEffect

--- monsters-rolodex/src/App.jsx
+++ monsters-rolodex/src/App.jsx
@@ -1,3 +1,4 @@
+import {useState,useEffect} from 'react'
 import './App.css'
 
 const Card = (props) => {
@@ -5,16 +6,27 @@ const Card = (props) => {
 };
 
 const App = () => {
-  const arr = ['a', 'b', 'c','d','e','f'];
+  const [arr, setArr] = useState(['a', 'b', 'c','d','e','f']);
+
+  useEffect(() => {
+    console.log('running side effect');
+  }, [])
 
   return (
     <>
     {
       arr.map((name, index) => {
-        console.log(`${name}-${index}`)
         return (
           <Card key={`${name}-${index}`} name={name} />
         )})}
+        <button 
+        onClick={() => {
+            setArr((arr) => [...arr, 'x']);
+            }
+          }
+        >
+          Add letter
+        </button>
     </>
   );
 }
